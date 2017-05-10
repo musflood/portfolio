f3f6d4987b3e5f116c4d549da2090fd2bd2aacb0
Megan Flood <mak.flood@comcast.net>
2017-05-10 08:01:45 -0700

    made the nav hide and show on clicking the arrow for mobile view and always show on desktop.

--- scripts/project.js
+++ scripts/project.js
@@ -1,8 +1,15 @@
 'use strict';
 
-function Project(rawProjectObj) {
-  this.img = rawProjectObj.img || 'default img';
-  this.title = rawProjectObj.title;
-  this.url = rawProjectObj.url;
-  this.description = rawProjectObj.description;
-}
+$(document).ready(function() {
+
+  $('.fa-chevron-circle-up').click(function() {
+    $('nav').toggleClass('hide');
+  });
+
+  function Project(rawProjectObj) {
+    this.img = rawProjectObj.img || 'default img';
+    this.title = rawProjectObj.title;
+    this.url = rawProjectObj.url;
+    this.description = rawProjectObj.description;
+  }
+});
