902d2d1afaafd502af1853ca24fbbf601eb7bb87
Megan Flood <mak.flood@comcast.net>
2017-05-11 16:05:14 -0700

    added handlebars template, more styling to the name-card, and made the page scroll to the correct position on nav click. fixed the devicon url.

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -1,10 +1,25 @@
 'use strict';
 
-$(document).ready(function() {
+var menuView = {}
 
+menuView.handleArrowClick = function() {
   $('.fa-chevron-circle-up').on('click',function(e) {
     e.preventDefault();
     $('nav').toggleClass('hide');
   });
+};
 
+menuView.handleTabClick = function() {
+  $('nav').on('click', '.tab',function(e) {
+    e.preventDefault();
+    console.log('click', $(this).data('locate'));
+    $('html, body').animate({
+      scrollTop: ($(`#${$(this).data('locate')}`).offset().top)
+    }, 100);
+  });
+};
+
+$(document).ready(function() {
+  menuView.handleArrowClick();
+  menuView.handleTabClick();
 });
