8c86a3bced3e06f97d54d1471281bc20324a51e9
Megan Flood <mak.flood@comcast.net>
2017-05-11 19:35:41 -0700

    added blinking to the cursor and typing out the heading.

--- scripts/projectView.js
+++ scripts/projectView.js
@@ -10,16 +10,47 @@ menuView.handleArrowClick = function() {
 };
 
 menuView.handleTabClick = function() {
-  $('nav').on('click', '.tab',function(e) {
-    e.preventDefault();
-    console.log('click', $(this).data('locate'));
+  $('nav').on('click', '.tab',function() {
     $('html, body').animate({
       scrollTop: ($(`#${$(this).data('locate')}`).offset().top)
-    }, 100);
+    }, 250);
   });
 };
+
+menuView.typeOutWords = function($element) {
+  // $element.hide();
+  var allText = $element.html();
+  console.log(allText);
+  var letters = [];
+  var foundTag = 0;
+  var letterWithBreak = '';
+  for (var i = 0; i < allText.length; i++) {
+    foundTag += allText[i+1] === '<' ? 1 : 0;
+    console.log(foundTag);
+    if(!foundTag) {
+      letters.push(allText[i]);
+    } else {
+      letterWithBreak += allText[i];
+      if (foundTag === 2 && allText[i] === '>') {
+        letters.push(letterWithBreak);
+        letterWithBreak = '';
+        foundTag = 0;
+      }
+    }
+  }
+  console.log(letters);
+  i = 0;
+  var interval = setInterval(function() {
+    $element.html(letters.slice(0, i).join(''));
+    if (i === letters.length) {
+      clearInterval(interval);
+    }
+    i++;
+  }, 300)
+}
 
 $(document).ready(function() {
   menuView.handleArrowClick();
   menuView.handleTabClick();
+  menuView.typeOutWords($('#name-card .text-to-write'));
 });
