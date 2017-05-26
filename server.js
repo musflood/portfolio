3e34592d4d23e0aa2e530d1771079e65a0d8a5be
Megan Flood <mak.flood@comcast.net>
2017-05-25 21:50:04 -0700

    changed the GitHub API request to be through a proxy. changed the social icons.

--- server.js
+++ server.js
@@ -1,15 +1,41 @@
 'use strict';
-// REQUIRES
+
+// REQUIRES //
+
 const express = require('express');
-// INSTANTIATION
+const requestProxy = require('express-request-proxy');
+
+// INSTANTIATION //
+
 const app = express();
-// SET-UP
+
+// SET-UP //
+
 const PORT = process.env.PORT || 3000;
 app.use(express.static('./public'));
-// ROUTES
+
+function proxyGithub(req, res) {
+  (requestProxy({
+    url:`https://api.github.com/${req.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(req, res);
+}
+
+// ROUTES //
+
+// navigation for tabs
 app.get('/info', function(req, res) { res.sendFile('index.html', {root:'./public'}) });
 app.get('/projects', function(req, res) { res.sendFile('index.html', {root:'./public'}) });
-// START SERVER
+
+// proxied authenticated request to GitHub
+app.head('/github/*', proxyGithub);
+app.get('/github/*', proxyGithub);
+
+// 404 error
+
+
+// START SERVER //
+
 app.listen(PORT, function() {
   console.log('Now serving on port', PORT);
 });
