07f5bb8a28cab29437e94e54198700aa6ae52d28
Megan Flood <mak.flood@comcast.net>
2017-07-03 17:40:23 -0700

    Created boilerplate for the new console view.

---
+++ public/scripts/views/consoleView.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+
+  // create a library for funcitons on the console view
+  const consoleView = {};
+
+  consoleView.initConsole = function() {
+
+  };
+
+  module.consoleView = consoleView;
+
+})(app);
